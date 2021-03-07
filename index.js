26c7b1ba69de39d29c07d68c7994c21584d8a00c
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2021-03-07 19:39:26 +0100

    products links

--- index.js
+++ index.js
@@ -1,19 +1,63 @@
 const fs = require('fs');
 const path = require('path');
 
-const scrapper = new (require('./catsScrapper'))();
+const categoriesScrapper = new (require('./catsScrapper'))();
+const ProductsScrapper = require('./productLinksScrapper');
 
+const CATEGORIES_FILE_NAME = path.join(__dirname, 'data', 'categories.json');
+const PRODUCTS_FILE_NAME = path.join(__dirname, 'data', 'products.json');
+const MAX_PAGES = 5;
 
-(
-    async function () {
-        const categoryURLs = await scrapper.run();
+async function getCats() {
+    const categoryURLs = await categoriesScrapper.run();
 
-        const dataFileName = path.join(__dirname, 'data' ,'categories.json');
-        const jsonData = JSON.stringify(categoryURLs);
+    fs.writeFile(CATEGORIES_FILE_NAME, JSON.stringify(categoryURLs), (err) => {
+        if (err) throw err;
+        else console.log('cats written...');
+    });
 
-        fs.writeFile(dataFileName, jsonData, (err) => {
+    return categoryURLs;
+}
+
+async function getProducts() {
+    fs.readFile(CATEGORIES_FILE_NAME, async (err , data) => {
+        if (err) throw err;
+
+        const prods = [];
+        // const cats = JSON.parse(data).categories;
+        const cats = [
+            "https://mediamarkt.es/es/category/_m%C3%B3viles-xiaomi-759538.html",
+            "https://mediamarkt.es/es/category/_black-friday-706013.html",
+            "https://mediamarkt.es/es/category/_lavadoras-samsung-762527.html"
+        ]
+
+        for (let index = 0; index < 3; index++) {
+            // const pos = Math.floor(Math.random() * cats.length) + 1;
+            const pos = index;
+            
+            const cat = cats[pos];
+            console.log(`\ngetting url at ${pos}\turl: ${cat}`);
+
+            const srp = new ProductsScrapper(cat, MAX_PAGES);
+            const links = await srp.getProductLinks();
+
+            prods.push({
+                cat,
+                links
+            });
+        }
+
+        fs.writeFile(PRODUCTS_FILE_NAME, JSON.stringify(prods), (err) => {
             if (err) throw err;
-            else console.log('cats written...');
+            else console.log('prods written...');
         });
-    }
-)()
+
+        return prods;
+    })
+}
+
+// getCats()
+//     .then(data => console.log(data));
+
+getProducts()
+    .then(data => console.log(data));
---
+++ productLinksScrapper.js
@@ -0,0 +1,68 @@
+const puppeteer = require('puppeteer');
+
+class ProductLinksScrapper {
+
+    constructor(categoryLink, maxPages) {
+        this.categoryLink = categoryLink;
+        this.maxPages = maxPages;
+
+        console.log(`initialized scrapper for link: ${categoryLink}`);
+    }
+
+    getProductLinks = async () => {
+        const browser = await puppeteer.launch({
+            slowMo: 300,
+            headless: false,
+            defaultViewport: {
+                height: 700,
+                width: 1300
+            }
+        });
+        const page = await browser.newPage();
+        await page.goto(this.categoryLink);
+
+        const acceptarTodo = await page.$("button[data-test='pwa-consent-layer-accept-all']");
+        if (acceptarTodo != null) {
+            await acceptarTodo.click();
+        }
+
+        const btn = await page.$("button[data-test='mms-search-srp-loadmore']");
+        if (btn != null) {
+            btn.click();
+
+            while (this.maxPages > 0) {
+                console.log('loading next page');
+                await page.evaluate(() => {
+                    const last = document.querySelector("div[data-test='mms-search-srp-productlist-item']:last-child");
+                    last.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+                })
+                
+                await page.waitForTimeout(3000);
+                this.maxPages--;
+            }
+
+        }
+
+
+        const data = await page.evaluate(() => {
+            const urls = [];
+
+            document.querySelectorAll('a').forEach(url => {
+                if (url.href.startsWith('https://www.mediamarkt.es/es/product')) {
+                    urls.push(url.href);
+                }
+            });
+
+            return urls;
+        });
+
+        await browser.close();
+
+        console.log(`got ${data.length} urls\n`);
+        return data;
+    }
+
+}
+
+
+module.exports = ProductLinksScrapper;
