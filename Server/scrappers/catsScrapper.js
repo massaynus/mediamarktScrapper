6794e8e4d7b254db56eca5f14d61594b66f79010
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2021-03-08 23:48:33 +0100

    extract category data

--- Server/scrappers/catsScrapper.js
+++ Server/scrappers/catsScrapper.js
@@ -1,3 +1,4 @@
+import cheerio from 'cheerio';
 import getUrls from 'get-urls';
 import fetch from 'node-fetch';
 
@@ -37,6 +38,17 @@ class MediamarktCategoriesScrapper {
         }
         else {
             console.log('status code: ', res.status);
+        }
+    }
+
+    extractCategoryData = async (parent, url) => {
+        const res = await fetch(url);
+        if (res.ok) {
+            const $ = cheerio.load(await res.text());
+            const name = $('#root > div.indexstyled__StyledAppWrapper-sc-1hu9cx8-0.klAfyt > main > div.Grid__StyledGrid-fs0zc2-0.cQIsoQ > div > div.Cellstyled__StyledCell-sc-1wk5bje-0.htWuLc > div.HeadLayout__StyledHead-kzl61k-0.ktKwWx > div.HeadLayout__StyledHeadline-kzl61k-3.jtoYfq > div > h1').text();
+            const productsCount = parseInt($("#root > div.indexstyled__StyledAppWrapper-sc-1hu9cx8-0.klAfyt > main > div.Grid__StyledGrid-fs0zc2-0.cQIsoQ > div > div.Cellstyled__StyledCell-sc-1wk5bje-0.htWuLc > div.HeadLayout__StyledHead-kzl61k-0.ktKwWx > div.HeadLayout__StyledHeadline-kzl61k-3.jtoYfq > div > span").text().substring(1).split(' ')[0]);
+
+            return {url, name, productsCount, parent_url: parent};
         }
     }
 
