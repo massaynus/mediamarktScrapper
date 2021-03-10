c13bc6d125e7a9dc638a8a6614834336a038457c
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2021-03-10 19:34:02 +0100

    routing

--- Client/simple-client/src/App.js
+++ Client/simple-client/src/App.js
@@ -1,10 +1,44 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { Container, Row, Col, Nav, Navbar, NavItem } from 'react-bootstrap';
 
+import Browse from './Components/Browse/Browse';
+import Dashboard from './Components/Dashboard/Dashboard';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
     return (
-        <h1>Hello</h1>
+        <Router >
+            <Container>
+                <Nav>
+                    <Navbar>
+                        <NavItem>
+                            <Link to="/dashboard" className="nav-link">Dashboard</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to="/" className="nav-link">Home</Link>
+                        </NavItem>
+                    </Navbar>
+                </Nav>
+
+                <hr />
+
+                <Row>
+                    <Col>
+                        <Switch>
+                            <Route exact path="/">
+                                <Browse />
+                            </Route>
+                            <Route path="/dashboard">
+                                <Dashboard />
+                            </Route>
+                        </Switch>
+                    </Col>
+                </Row>
+
+            </Container>
+        </Router>
     )
 }
 
